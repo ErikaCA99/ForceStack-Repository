dd38adcaeb272a27e563256a4f88e9ce5ad5dba2
Erika Chino <erika.53chinoalanoca@gmail.com>
2025-10-21 23:40:45 -0400

    Configurar GitHub Actions con ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,21 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, extends: ["js/recommended"], 
+    languageOptions: { 
+        globals: {
+          ...globals.browser,
+          ...globals.node
+        }
+    },
+    extends: [js.configs.recommended],
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+      "semi" : ["error", "always"],
+      "quotes": ["error", "double"]
+    }
+  },
 ]);
